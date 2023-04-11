3a16396c73342248dedaf288f8504e881b28e962
JoanGomez <joangomezusma4@gmail.com>
2023-04-10 23:12:34 -0500

    se agregan componentes para el formulario

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import styled from "styled-components";
+import { Formik, Form } from "formik";
+import Input from './components/Input';
+import Button from './components/Button'
+import Container from './components/Container'
+import Section from './components/Section'
 
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100%;
-  align-items: center;
-`
-
-const Section = styled.section`
-  background-color: #eee;
-  border-top: solid 2px palevioletred;
-  padding: 20px 25px;
-  width: 500px;
-  box-shadow: 0px 2px 3px rgb(0,0,0,0.3);
-`
 
 function App() {
+
+  const handleSubmit = () => {
+
+  }
+
   return (
 
     <Container>
       <Section>
-      lala
+        <Formik initialValues={{deposit: "", contribution: "", years: "", rate: "",  }} onSubmit ={handleSubmit}>
+
+          <Form>
+            <Input name="deposit" label ="Deposito Inicial"/>
+            <Input name="contribution" label ="Contribucion Anual"/>
+            <Input name="years" label ="Años"/>
+            <Input name="rate" label ="Interes Estimado"/>
+            <Button>Calcular</Button>
+  
+          </Form>
+
+        </Formik>
       </Section>
     </Container>
     
