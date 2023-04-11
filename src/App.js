fb42de5ed370cceb244ec3eb80d90e6be63698ac
JoanGomez <joangomezusma4@gmail.com>
2023-04-10 23:35:07 -0500

    Creacion de componente para el procesamiento y formulacion de los datos recibidos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import { Formik, Form } from "formik";
 import Input from './components/Input';
 import Button from './components/Button'
 import Container from './components/Container'
 import Section from './components/Section'
 
+const compoundInterest = ( deposit, contribution, years, rate) =>{
 
+  let total = deposit
+
+  for (let i = 0 ; i< years ; i++){
+
+    total = (total + contribution)* (rate + 1)
+  }
+
+  return Math.round(total)
+
+
+}
 
 function App() {
 
-  const handleSubmit = () => {
+  const [balance, setBalance] = useState("")
+
+  const handleSubmit = ({deposit, contribution, years, rate}) => {
+
+    const val = compoundInterest(Number(deposit), Number(contribution),Number(years), Number(rate))
+    setBalance(val)
 
   }
 
@@ -28,6 +46,8 @@ function App() {
           </Form>
 
         </Formik>
+
+        {balance !== "" ? `Balance Final: ${balance}`: null}
       </Section>
     </Container>
     
